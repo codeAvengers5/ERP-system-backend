c112cedb253bb3bf506a08481f9e1bbe44e36a31
EdlawitG <eduedil30@gmail.com>
2024-01-15 19:17:14 +0300

    commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require('dotenv').config('../')
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`DB connected Succesfully`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { connectDB };
