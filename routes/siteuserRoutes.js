9aded872a2e99c58cd590992d6ea8cb4ebf66a39
EdlawitG <eduedil30@gmail.com>
2024-03-02 10:11:21 +0300

    adding_job_route

--- routes/siteuserRoutes.js
+++ routes/siteuserRoutes.js
@@ -7,8 +7,6 @@ const {
   ResetPassword,
   UpdatePassword,
 } = require("../controllers/siteuser-contollers");
-const { ViewJob, JobApply } = require("../controllers/jobapply-contollers");
-const { uploadCV } = require("../config/multer");
 const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Welcome");
@@ -19,6 +17,4 @@ router.post("/confirmemail", ConfirmEmail);
 router.post("/forgot-password", ForgotPassword);
 router.post("/reset_password/:id/:token", ResetPassword);
 router.post("/update_password/:id", UpdatePassword);
-router.get("/joblist", ViewJob);
-router.post("/jobapply/:id", uploadCV.single("cv"), JobApply);
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const { connectDB } = require("./config/db");
 const adminUserRoute = require("./routes/adminusersRoutes");
 const siteUserRoute = require("./routes/siteuserRoutes");
+const jobRoute = require("./routes/jobRoutes");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 require("dotenv").config();
@@ -15,10 +16,12 @@ app.use(
     secret: "your-secret-key",
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 30 * 60 * 1000 }
   })
 );
 app.use(adminUserRoute);
 app.use(siteUserRoute);
+app.use(jobRoute)
 connectDB()
   .then(() => {
     app.listen(process.env.PORT, () => {
