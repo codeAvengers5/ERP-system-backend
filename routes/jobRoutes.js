4ca14a154044ddf044b93ac284bdc16ed665ac25
EdlawitG <eduedil30@gmail.com>
2024-03-02 17:20:31 +0300

    job_summary

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { ViewJob, JobApply } = require("../controllers/jobapply-contollers");
+const { ViewJob, JobApply, JobSummary } = require("../controllers/jobapply-contollers");
 const { uploadCV } = require("../config/multer");
+const { isAuthenticated, isHRAdmin } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/joblist", ViewJob);
 router.post("/jobapply/:id", uploadCV.single("cv"), JobApply);
+router.get("/jobsummary", isAuthenticated,isHRAdmin,JobSummary)
 module.exports = router;
