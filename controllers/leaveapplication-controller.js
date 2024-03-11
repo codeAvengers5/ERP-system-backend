4aae2f87c2074f35a08933b16b4402ff4a54d7cd
EdlawitG <eduedil30@gmail.com>
2024-03-12 01:47:13 +0300

    some_changes

---
+++ controllers/leaveapplication-controller.js
@@ -0,0 +1,126 @@
+const LeaveApplication = require("../models/leaveApplication");
+async function createLeaveApplication(req, res) {
+  try {
+    const { full_name, duration, leave_date, detail } = req.body;
+    const employee_id = req.user.id; //employee_id
+    const leaveApplication = new LeaveApplication({
+      employee_id,
+      full_name,
+      duration,
+      leave_date,
+      detail,
+      status: "pending",
+    });
+    await leaveApplication.save();
+    res.status(201).json({ message: "Leave application created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function getLeaveApplication_forEmployee(req, res) {
+  try {
+    const leaveApplication = await LeaveApplication.find({
+      employee_id: req.user.id,
+    });
+    if (!leaveApplication) {
+      return res.status(404).json({ message: "Leave application not found" });
+    }
+    return res.status(200).json(leaveApplication);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function getAllLeaveApplications_forHR(req, res) {
+  try {
+    const leaveApplications = await LeaveApplication.find();
+    res.status(200).json(leaveApplications);
+    return res.status(403).json({ message: "Access forbidden" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function updateLeaveApplication(req, res) {
+  try {
+    const { id } = req.params; //leaveappliction
+    const { full_name, duration, leave_date, detail } = req.body;
+
+    const updatedApplication = await LeaveApplication.findByIdAndUpdate(id, {
+      full_name: full_name,
+      duration: duration,
+      leave_date: leave_date,
+      detail: detail,
+    });
+    if (!updatedApplication) {
+      return res.status(404).json({ message: "Leave application not found" });
+    }
+    res.status(200).json({ message: "Leave application updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function updateStatus(req, res) {
+  try {
+    const { id } = req.params; //leave_application_id
+    const { status } = req.body;
+    const leaveApplication = await LeaveApplication.findById(id);
+    if (!leaveApplication) {
+      return res.status(404).json({ message: "Leave application not found" });
+    }
+    leaveApplication.status = status;
+    await leaveApplication.save();
+
+    return res.status(200).json(leaveApplication);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function deleteLeaveApplication(req, res) {
+  try {
+    const { id } = req.params;
+    if (req.user.role === "employee") {
+      await LeaveApplication.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ message: "Leave application deleted successfully" });
+    } else {
+      return res.status(404).json({ message: "Not authorized" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+async function filterByStatus(req, res) {
+  try {
+    const { status } = req.query;
+
+    let leaveApplications;
+    if (status) {
+      if (!["pending", "approved", "rejected"].includes(status)) {
+        return res.status(400).json({ message: "Invalid status" });
+      }
+      leaveApplications = await LeaveApplication.find({ status });
+    } else {
+      leaveApplications = await LeaveApplication.find();
+    }
+
+    return res.status(200).json(leaveApplications);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+module.exports = {
+  createLeaveApplication,
+  getLeaveApplication_forEmployee,
+  filterByStatus,
+  updateLeaveApplication,
+  updateStatus,
+  deleteLeaveApplication,
+  getAllLeaveApplications_forHR,
+};
--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -3,24 +3,59 @@ const router = express.Router();
 
 const {
   createLeaveApplication,
-  getAllLeaveApplications,
-  getLeaveApplicationById,
   updateLeaveApplication,
   deleteLeaveApplication,
-  getAllLeaveApplicationsByEmployee,
-} = require("../controllers/leave-controller");
-const { isAuthenticated, isEmployee ,isHRAdmin} = require("../middleware/auth");
+  getLeaveApplication_forEmployee,
+  getAllLeaveApplications_forHR,
+  updateStatus,
+  filterByStatus,
+} = require("../controllers/leaveapplication-controller");
+const {
+  isAuthenticated,
+  isEmployee,
+  isHRAdmin,
+} = require("../middleware/auth");
 
-
-router.post("/leaveapplications/:id",isAuthenticated, isEmployee, createLeaveApplication);
-
-router.get("/leaveapplications",isAuthenticated, isHRAdmin, getAllLeaveApplications);
-
-router.get("/leaveapplications/:id", isAuthenticated,[isHRAdmin, isEmployee],getLeaveApplicationById);
-
-router.put("/leaveapplications/:id",isAuthenticated, [isHRAdmin, isEmployee], updateLeaveApplication);
-
-router.delete("/leaveapplications/:id",isAuthenticated, isEmployee, deleteLeaveApplication);
-
-router.get("/leaveapplications/employee/:id", isAuthenticated, [isHRAdmin, isEmployee], getAllLeaveApplicationsByEmployee);
+router.post(
+  "/leaveapplications",
+  isAuthenticated,
+  isEmployee,
+  createLeaveApplication
+);
+router.get(
+  "/leaveapplication_filter",
+  isAuthenticated,
+  isHRAdmin,
+  filterByStatus
+);
+router.get(
+  "/leaveapplications_hr",
+  isAuthenticated,
+  isHRAdmin,
+  getAllLeaveApplications_forHR
+);
+router.get(
+  "/leaveapplications_employee",
+  isAuthenticated,
+  isEmployee,
+  getLeaveApplication_forEmployee
+);
+router.put(
+  "/leaveapplications/:id",
+  isAuthenticated,
+  isEmployee,
+  updateLeaveApplication
+);
+router.put(
+  "/leaveapplication_status/:id",
+  isAuthenticated,
+  isHRAdmin,
+  updateStatus
+);
+router.delete(
+  "/leaveapplications/:id",
+  isAuthenticated,
+  isEmployee,
+  deleteLeaveApplication
+);
 module.exports = router;
