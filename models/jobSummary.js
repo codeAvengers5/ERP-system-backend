572c851e5becea2c642f5a4e22814ea41732d873
EdlawitG <eduedil30@gmail.com>
2024-03-17 14:04:19 +0300

    adding_TTL

--- models/jobSummary.js
+++ models/jobSummary.js
@@ -1,38 +1,46 @@
 const mongoose = require("mongoose");
 
-const jobSummarySchema = new mongoose.Schema({
-  job_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "JobPost",
-    required: true,
+const jobSummarySchema = new mongoose.Schema(
+  {
+    job_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "JobPost",
+      required: true,
+    },
+    full_name: {
+      type: String,
+      required: true,
+    },
+    cv: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone_no: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    expireAt: {
+      type: Date,
+      default: Date.now,
+      index: { expires: "1m" },
+    },
   },
-  full_name: {
-    type: String,
-    required: true,
-  },
-  cv: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone_no: {
-    type: String,
-    required: true,
-  },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "approved", "rejected"],
-    default: "pending",
-  },
-});
+  { timestamps: true }
+);
 
 const JobSummary = mongoose.model("JobSummary", jobSummarySchema);
 
