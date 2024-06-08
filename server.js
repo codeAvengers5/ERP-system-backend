1e20c8e0110013e72e5206f718f80348228243b1
Edlawit G <eduedil30@gmail.com>
2024-06-08 16:41:49 +0300

    fixing_cors

--- server.js
+++ server.js
@@ -21,17 +21,16 @@ const { performCheckIn } = require("./controllers/attendance-controllers.js");
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "http://localhost:3001",
-//       "https://user-site-production.up.railway.app/",
-//     ],
-//     credentials: true,
-//   })
-// );
-app.use(cors({origin:'https://user-site-production.up.railway.app'}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://user-site-production.up.railway.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("welcome");
